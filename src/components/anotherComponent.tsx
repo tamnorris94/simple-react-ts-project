787a9790f6c0333422352251bd16103c438b0a00
TamNorris <tam_norris@yahoo.com>
2024-03-08 11:45:46 +0000

    Argh whatever

--- src/components/anotherComponent.tsx
+++ src/components/anotherComponent.tsx
@@ -5,11 +5,7 @@ const doNothing = () => {
 export const AnotherComponent = () => {
     return (
     <div>
-<<<<<<< HEAD
-        <h3>New Heading in Main</h3>
-=======
         <h3>A New Heading</h3>
->>>>>>> 39d0270 (Changed Heading in tambr4)
         <p>A paragraph</p>
         <p>Woops added this as well.</p>
         <p>Yet another para</p>
