e009d55211c1535b96ffa6b30c1b4575bab695cd
TamNorris <tam_norris@yahoo.com>
2024-03-22 15:52:36 +0000

    Farout

--- src/components/anotherComponent.tsx
+++ src/components/anotherComponent.tsx
@@ -5,7 +5,12 @@ const doNothing = () => {
 export const AnotherComponent = () => {
     return (
     <div>
-        <h3>Tambr5</h3>
+        <h3>New Heading in Main</h3>
+        <p>A paragraph</p>
+        <p>Woops added this as well.</p>
+        <p>Yet another para</p>
+        <p>Add a para while in main</p>
+        <p>Add poopy</p>
     </div>
     )
 }   
